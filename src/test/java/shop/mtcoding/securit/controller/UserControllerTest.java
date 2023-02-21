e7e45b4865faddae3b03291744f805d8135ddaa7
kim ho hyeon <khh5762@naver.com>
2023-02-21 11:10:38 +0900

    회원가입 완료

---
+++ src/test/java/shop/mtcoding/securit/controller/UserControllerTest.java
@@ -0,0 +1,48 @@
+package shop.mtcoding.securit.controller;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import javax.servlet.http.HttpSession;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.transaction.annotation.Transactional;
+
+import shop.mtcoding.securit.model.User;
+
+/*
+ * SpringBootTest는 통합테스트 (실제 환경과 동일하게 Bean이 생성됨)
+ * AutoConfigureMockMvc는 Mock 환경의 Ioc 컨테이너에 Mockmvc Bean이 생성됨
+ * 테스트는 순서가 뭐가먼저 실행될지 모른다.
+ */
+
+@Transactional
+@AutoConfigureMockMvc
+@SpringBootTest(webEnvironment = WebEnvironment.MOCK)
+public class UserControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void join_test() throws Exception {
+        // given
+        String requestBody = "username=ssar11&password=1234&email=ssar@nate.com";
+
+        // when
+        ResultActions resultActions = mvc.perform(post("/join").content(requestBody)
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED_VALUE));
+
+        // then
+        resultActions.andExpect(status().is3xxRedirection());
+    }
+
+}
