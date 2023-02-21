6d289b6d871001053ab2494b288650b5f806d68f
kim ho hyeon <khh5762@naver.com>
2023-02-21 09:56:52 +0900

    view 세팅 및 경로 설정 완료

--- src/main/java/shop/mtcoding/securit/controller/UserController.java
+++ src/main/java/shop/mtcoding/securit/controller/UserController.java
@@ -1,5 +1,23 @@
 package shop.mtcoding.securit.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class UserController {
 
+    @GetMapping("/main")
+    public String main() {
+        return "user/main";
+    }
+
+    @GetMapping("/loginForm")
+    public String loginForm() {
+        return "user/loginForm";
+    }
+
+    @GetMapping("/joinForm")
+    public String joinForm() {
+        return "user/joinForm";
+    }
 }
